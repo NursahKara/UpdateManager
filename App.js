70e8ddba9f494c4b4ede75c4784fa596d3b6654b
NursahKara <nursahh.kara@gmail.com>
2021-04-26 16:40:26 +0300

    add apk link

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ export default class App extends Component {
   updateApk = () => {
     try {
 
-      var filePath = RNFS.CachesDirectoryPath + '/Youtube_go.com.apk';
+      var filePath = RNFS.CachesDirectoryPath + '/com.sample.apk';
       var download = RNFS.downloadFile({
-        fromUrl: 'https://www.apkmirror.com/wp-content/uploads/2021/04/14/60838f8f4dada/com.google.android.youtube_16.15.36-1520428480_minAPI21(arm64-v8a,armeabi-v7a,x86,x86_64)(nodpi)_apkmirror.com.apk?verify=1619301641-qeb4WP3uCcLvnAe9-UyeJA7ZUDqld5WfKmy8R3EExyk',
+        fromUrl: 'https://download.wetransfer.com//eugv/5e932b07bfdd9da0185023f06f1d234a20210426133607/d0ad858b4c10a6c976b62cda85ac908ab011d33d/com.sony.nfx.app.sfrc_5.2.26.30.2-16522630_minAPI22%28nodpi%29_apkmirror.com.apk?cf=y&token=eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTk0NDQ4NDMsInVuaXF1ZSI6IjVlOTMyYjA3YmZkZDlkYTAxODUwMjNmMDZmMWQyMzRhMjAyMTA0MjYxMzM2MDciLCJmaWxlbmFtZSI6ImNvbS5zb255Lm5meC5hcHAuc2ZyY181LjIuMjYuMzAuMi0xNjUyMjYzMF9taW5BUEkyMihub2RwaSlfYXBrbWlycm9yLmNvbS5hcGsiLCJ3YXliaWxsX3VybCI6Imh0dHA6Ly9wcm9kdWN0aW9uLmJhY2tlbmQuc2VydmljZS5ldS13ZXN0LTEud3Q6OTI5Mi93YXliaWxsL3YxL3Nhcmthci85MDZiYWY3Yzk4ZGMxN2ZhYjhiNWYwYzI2NzE5NjRhMDIwMzYxODk3ZDIxOTAwMzQ0ZjBkYmQ4ZGM5NGZlZDAyNTdkYWZmZTg0OTkxMjIxZmJhOTY3MCIsImZpbmdlcnByaW50IjoiZDBhZDg1OGI0YzEwYTZjOTc2YjYyY2RhODVhYzkwOGFiMDExZDMzZCIsImNhbGxiYWNrIjoie1wiZm9ybWRhdGFcIjp7XCJhY3Rpb25cIjpcImh0dHA6Ly9wcm9kdWN0aW9uLmZyb250ZW5kLnNlcnZpY2UuZXUtd2VzdC0xLnd0OjMwMDAvd2ViaG9va3MvYmFja2VuZFwifSxcImZvcm1cIjp7XCJ0cmFuc2Zlcl9pZFwiOlwiNWU5MzJiMDdiZmRkOWRhMDE4NTAyM2YwNmYxZDIzNGEyMDIxMDQyNjEzMzYwN1wiLFwiZG93bmxvYWRfaWRcIjoxMTk3MzAyNzUzMH19In0.xVh6r9xSd_Prt1a8LMcIdUJjxEa9upsZwoMT-KirbtI',
         toFile: filePath,
         progress: res => {
           console.log(((res.bytesWritten / res.contentLength) * 100).toFixed(2) + '%');
